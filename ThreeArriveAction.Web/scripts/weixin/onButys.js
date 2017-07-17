4cc8eb6b8b0cac6b34f472aaf822fd4022e404ae
孙诚 <sunchengsuncheng@gmail.com>
2017-07-17 17:40:18 +0800

    迁入:微信值班功能

---
+++ ThreeArriveAction.Web/scripts/weixin/onButys.js
@@ -0,0 +1,43 @@
+﻿var vm = new Vue({
+    el: "#app",
+    data: {
+        ddlvillage: -1,
+        page: 0,
+        size: 5,
+        totle: 0,
+        list: []
+    },
+    methods: {
+        getpage: function (page) {
+            var that = this;
+            $.ajax({
+                type: "post",
+                url: "../Ajax/sys_OnButysManager.ashx?type=getOnButysList",
+                data: {
+                    page: page,
+                    size: that.size,
+                    ddlvillage: that.ddlvillage
+                },
+                complete: function (d) {
+                    var obj = JSON.parse(d.responseText);
+                    that.page = obj.page;
+                    that.totle = obj.totle;
+                    that.list = obj.list;
+                    if (that.page > 1) {
+                        $("#lastpage").attr("disabled", false);
+                    } else {
+                        $("#lastpage").attr("disabled", true);
+                    }
+                    if (that.page < (that.totle / that.size)) {
+                        $("#nextpage").attr("disabled", false);
+                    } else {
+                        $("#nextpage").attr("disabled", true);
+                    }
+                }
+            });
+        }
+    },
+    mounted: function () {
+        this.getpage(1);
+    }
+});
---
+++ ThreeArriveAction.Web/scripts/weixin/onButysSet.js
@@ -0,0 +1,58 @@
+﻿var vm = new Vue({
+    el: "#app",
+    data: {
+        town: "",
+        village: "",
+        villageId: 0,
+        userValue: -1,
+        list: [],
+        exist: false
+    },
+    methods: {
+        submitcontent: function () {
+            var that = this;
+            if (that.userValue === -1) {
+                alert("请先选择值班人员");
+                return;
+            }
+            $.ajax({
+                type: "post",
+                url: "../Ajax/sys_OnButysManager.ashx?type=set",
+                data: {
+                    userid: that.userValue,
+                    villageid: that.villageId
+                },
+                complete: function (d) {
+                    var obj = JSON.parse(d.responseText);
+                    alert(obj.info);
+                    if (obj.status === "y") {
+                        window.open("onButys.html", "_self");
+                    }
+                }
+            });
+        }
+    },
+    mounted: function () {
+        var that = this;
+        $.ajax({
+            type: "post",
+            url: "../Ajax/sys_UsersManager.ashx?type=weixinGetButyUser",
+            data: {
+                openId: localStorage.getItem("openId")
+            },
+            complete: function (d) {
+                var obj = JSON.parse(d.responseText);
+                that.town = obj.town;
+                that.village = obj.village;
+                that.list = obj.list;
+                that.villageId = obj.villageId;
+                that.exist = obj.exist;
+                if (that.exist) {
+                    $("#btnSub").attr("disabled", true);
+                } else {
+                    $("#btnSub").attr("disabled", false);
+                }
+            }
+        });
+    }
+});
